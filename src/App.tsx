df780dd2b33bbbaac256713aa59b21d9bdb6e7ab
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2022-04-24 14:47:53 +0000

    added a router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import type { Component } from 'solid-js';
+import { Component, lazy } from 'solid-js';
+import { Route, Routes } from 'solid-app-router';
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+const Home = lazy(() => import('./pages/Home'));
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 };
 
