b663cb2fed5f2df725ef654f68ae72ac133f9b88
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2022-04-26 10:20:05 +0000

    added a basic service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
-import { Router } from 'solid-app-router';
+import { render } from "solid-js/web";
+import { Router } from "solid-app-router";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { register } from "./sw/register";
 
 render(() =>
     <Router>
         <App />
     </Router>,
-    document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
+
+register()
+    .then(() => fetch("/test-sw"))
+    .then(x => x.text())
+    .then(alert);
