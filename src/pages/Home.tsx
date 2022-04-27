442f90bff6b4c97642b3e400d0cbdaeeccc0ab07
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2022-04-27 10:34:56 +0000

    added a file input

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,20 @@
-import { Component, createSignal, onMount } from "solid-js";
+import { Component } from "solid-js";
+
+import { readFile } from "../utils";
 
 const Home: Component = () => {
-  const [text, setText] = createSignal("");
-  
-  onMount(async () => {
-    const response = await fetch("/test-sw");
-    setText(await response.text());
-  });
-
-  return <div>home {text()}</div>;
+  return (
+    <>
+      <h1>HAR viewer</h1>
+      <p>Upload a <code>.har</code> file or choose a previously loaded file</p>
+      <input type="file" accept=".har, application/json" onInput={upload} />
+    </>
+  );
 }
 
 export default Home;
+
+async function upload(event: InputEvent) {
+  let file = await readFile(event.currentTarget as HTMLInputElement);
+  alert(file);
+}
--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,20 @@ export function sleep(ms: number, throws = false): Promise<void> {
     setTimeout(throws ? reject : resolve, ms);
   });
 }
+
+export function readFile(element: HTMLInputElement): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const file = element.files?.[0];
+    if (!file) {
+      reject("Could not load the file");
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onerror = () => reject("Could not read the file");
+    fileReader.onload = () => {
+      resolve(fileReader.result as string);
+    };
+    fileReader.readAsText(file);
+  });
+}
