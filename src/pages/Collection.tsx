99ae3acb967a693ec9fc6e9783f8fa4db82d46f9
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2022-06-15 11:24:37 +0200

    added a back button in Collection

--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -1,9 +1,11 @@
 import { Link, useParams } from "solid-app-router";
 import { Component, createResource, For, Show } from "solid-js";
+import { icons } from "feather-icons";
 
 import styles from "./Collection.module.css";
 import { get } from "../db";
 import { noun } from "../utils";
+import Feather from "../Feather";
 
 const Collection: Component = () => {
   const encodedName = useParams().name;
@@ -13,6 +15,9 @@ const Collection: Component = () => {
   return (
     <>
       <h1>HAR viewer</h1>
+      <Link href="/" class={styles.back}>
+        <Feather icon={icons.x} />
+      </Link>
       <Show when={!pages.loading} fallback={<p>Loading...</p>}>
         <p>Found {pages()?.length} {noun("page", pages()?.length !== 1)} in {name}</p>
         <ul class={styles.list}>
