6257e44ad6aaf51f0face80e17bfe3e028dc0566
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2022-06-18 10:34:25 +0200

    added pathnameScore

--- src/sw/matchScore.ts
+++ src/sw/matchScore.ts
@@ -1,3 +1,5 @@
+import { last } from "../utils";
+
 /**
  * Gives a score based on how good the URLs match. A lower score means a
  * better match.
@@ -36,6 +38,48 @@ function paramsScore(requestedParams: URLSearchParams, otherParams: URLSearchPar
 
   // the remaining parameters are just in `otherParams` and not in `requestedParams`
   score += 2 * [...otherParams].length;
+
+  return score;
+}
+
+function pathnameScore(requestedPathname: string, otherPathname: string) {
+  const requestedPathComponents = requestedPathname.split("/");
+  const otherPathComponents = otherPathname.split("/");
+  const minLength = Math.min(requestedPathComponents.length, otherPathComponents.length);
+  const lengthDifference = requestedPathComponents.length - otherPathComponents.length;
+
+  let score = 0;
+
+  // if one of the urls ends with a slash, but they otherwise have the same
+  // number of components, add 0.25
+  if (lengthDifference === 1 && last(requestedPathComponents) === ""
+    || lengthDifference === -1 && last(otherPathComponents) === "")
+    score += 0.25;
+  else
+    // else add 2 for each component that is too many
+    score += 2 * Math.abs(lengthDifference);
+
+  for (let i = 0; i < minLength; i++) {
+    const value = requestedPathComponents[i];
+    const otherValue = otherPathComponents[i];
+
+    if (value === otherValue) continue;
+
+    if (value.length === otherValue.length)
+      score += 0.75;
+    else
+      score++;
+  }
+
+  // if the file extensions are the same, remove 0.25 of the score
+  const requestedFile = last(requestedPathComponents);
+  const otherFile = last(otherPathComponents);
+  if (requestedFile.includes(".") && otherFile.includes(".") && requestedFile !== otherFile) {
+    const requestedFileExtension = last(requestedFile.split("."));
+    const otherFileExtension = last(otherFile.split("."));
+
+    if (requestedFileExtension === otherFileExtension) score -= 0.25;
+  }
 
   return score;
 }
