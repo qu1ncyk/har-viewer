1b7cc112b49b51dbd42deaf7e4f41f18315c5fe5
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2022-07-05 18:46:19 +0200

    added a cache for the matching algotithm

--- src/sw/view.ts
+++ src/sw/view.ts
@@ -3,6 +3,9 @@ import type { Entry } from "../db/db";
 import { rewrite } from "../rewrite/sw";
 import { matchScore } from "./matchScore";
 
+/** `originalUrl/collection, [otherUrl, Date]` */
+let matchCache = new Map<string, [string, Date]>();
+
 export async function view(url: URL) {
   // /view/:collection/:time/:originalUrl 
   const pathParts = url.pathname.split("/");
@@ -16,13 +19,23 @@ export async function view(url: URL) {
   const store = await get.entriesStore(collection);
   let lowestScore = Infinity;
   let bestMatch: Entry | undefined;
-  for await (const cursor of store) {
-    const entry = cursor.value;
-    const score = matchScore(new URL(originalUrl), new URL(entry.url));
-    if (score < lowestScore) {
-      lowestScore = score;
-      bestMatch = entry;
+
+  const key = matchCache.get(originalUrl + "/" + collection);
+
+  if (key) {
+    bestMatch = await store.get(key);
+  } else {
+    for await (const cursor of store) {
+      const entry = cursor.value;
+      const score = matchScore(new URL(originalUrl), new URL(entry.url));
+      if (score < lowestScore) {
+        lowestScore = score;
+        bestMatch = entry;
+      }
     }
+
+    if (bestMatch)
+      matchCache.set(originalUrl + "/" + collection, [bestMatch.url, bestMatch.time]);
   }
 
   return await rewrite(bestMatch, time, collection);
