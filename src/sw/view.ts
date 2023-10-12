58d7bc71a2da83203443a6c6b73f973e207a16b2
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2023-10-12 07:48:59 +0200

    Slightly refactored the view function

--- src/sw/view.ts
+++ src/sw/view.ts
@@ -1,42 +1,67 @@
-import { get } from "../db";
-import type { Entry } from "../db/db";
+import { openCollection, type Entry } from "../db/db";
 import { rewrite } from "../rewrite/sw";
 import { matchScore } from "./matchScore";
 
-/** `originalUrl/collection, [otherUrl, Date]` */
+/**
+ * The cache of the looked up URLs mapped to the best matches.
+ * `originalUrl/collection, matchedUrl`
+ */
 let matchCache = new Map<string, [string, Date]>();
 
+/**
+ * The function that handles calls to the /view/* URL path. Fetch the requested
+ * page from the collection in the database, rewrite it so that the references
+ * don't break and return a `Response`.
+ *
+ * @param url The URL of the requested page. The pathname should have the shape
+ * of `/view/:collection/:time/:originalUrl`.
+ */
 export async function view(url: URL) {
-  // /view/:collection/:time/:originalUrl 
-  const pathParts = url.pathname.split("/");
-  let [, , collection, time, ...originalUrlArray] = pathParts;
-  collection = decodeURIComponent(collection);
-  let originalUrl = originalUrlArray.join("/") + url.search;
-
-  if (originalUrl.startsWith("//"))
-    originalUrl = "https:" + originalUrl;
-
-  const store = await get.entriesStore(collection);
+  const { collectionName, originalUrl, time } = splitUrl(url);
+  const objectStore = await openCollection(collectionName);
+  const urlsStore = objectStore.transaction("urls").store;
   let lowestScore = Infinity;
-  let bestMatch: Entry | undefined;
+  let bestMatch: [string, Date] | undefined;
 
-  const key = matchCache.get(originalUrl + "/" + collection);
+  const cached = matchCache.get(originalUrl + "/" + collectionName);
 
-  if (key) {
-    bestMatch = await store.get(key);
+  if (cached) {
+    bestMatch = cached;
   } else {
-    for await (const cursor of store) {
-      const entry = cursor.value;
-      const score = matchScore(new URL(originalUrl), new URL(entry.url));
+    for await (const cursor of urlsStore) {
+      const url = cursor.key;
+      const score = matchScore(new URL(originalUrl), new URL(url));
       if (score < lowestScore) {
         lowestScore = score;
-        bestMatch = entry;
+        bestMatch = [url, cursor.value[0]];
       }
     }
 
     if (bestMatch)
-      matchCache.set(originalUrl + "/" + collection, [bestMatch.url, bestMatch.time]);
+      matchCache.set(originalUrl + "/" + collectionName, bestMatch);
   }
 
-  return await rewrite(bestMatch, time, collection);
+  let entry: Entry | undefined;
+  if (bestMatch) {
+    entry = await objectStore.get("entries", bestMatch);
+  }
+  return await rewrite(entry, time, collectionName);
+}
+
+/**
+ * Split a view URL into multiple parts.
+ *
+ * @param url The URL of the requested page. The pathname should have the shape
+ * of `/view/:collection/:time/:originalUrl`.
+ */
+function splitUrl(url: URL) {
+  const pathParts = url.pathname.split("/");
+  let [, , collectionName, time, ...originalUrlArray] = pathParts;
+  collectionName = decodeURIComponent(collectionName);
+  let originalUrl = originalUrlArray.join("/") + url.search;
+  if (originalUrl.startsWith("//")) {
+    originalUrl = "https:" + originalUrl;
+  }
+
+  return { collectionName, originalUrl, time };
 }
